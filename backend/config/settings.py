f929ee3d257b221d749538e2af76b2b4fcb91cc8
Sukriticodes <sukritidhakalofficial@gmail.com>
2026-08-21 21:23:32 +0545

    Prepare backend for Render deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,13 +29,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "10.0.2.2",
-    "192.168.254.195",
-    "192.168.1.75"
-]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost,10.0.2.2,192.168.254.195,192.168.1.75"
+).split(",")
 
 
 # Application definition
